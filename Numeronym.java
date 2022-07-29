caffc9c4bc1921d8bf532941b6b007c41d90b93e
Munaimah Khan <90429401+MunaimahK@users.noreply.github.com>
2022-07-29 16:49:43 -0400

    Numeronym.java
    Numeronym functionality code.

---
+++ Numeronym.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Numeronym {
+    private String input;
+
+    public void setInput(String input) {
+        this.input = input;
+    }
+
+    public Numeronym(String input) {
+        this.input = input;
+
+    }
+
+    public String printNumeronym(String input)
+    {
+        this.input = input;
+        char[] ch = input.toCharArray();
+        int num = ch.length - 2;
+        int length = ch.length;
+
+        if(input.contains(".") || input.contains("!") || input.contains("?"))
+        {
+            return "Invalid input.";
+        }
+        if (length >= 9)
+        {
+            return input +" -> " + ch[0] + num + ch[length-1];
+        }
+        else
+        {
+            return input + " (no change)";
+        }
+    }
+}
